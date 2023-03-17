cf47b7f5a23ecb51c4989ec0058036af3090c7d7
shadow0419 <actionemail0419@gmail.com>
2023-03-18 04:51:39 +0800

    feat : bird handler

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
+
 	r.HandleFunc("/hello", handler).Methods("GET")
+	r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
 
 	staticFileDirectory := http.Dir("./assets/")
-
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
-
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
 
 	return r
 }
 
 func main() {
-
 	r := newRouter()
 	http.ListenAndServe(":8080", r)
 }
