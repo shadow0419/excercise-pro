6bc362a738560e55e9e4d44a19377d135cdf0d62
shadow0419 <actionemail0419@gmail.com>
2023-03-18 04:11:17 +0800

    feat : testing the static file

--- main.go
+++ main.go
@@ -7,12 +7,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func main() {
-
+func newRouter() *mux.Router {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/hello", handler).Methods("GET")
 
+	staticFileDirectory := http.Dir("./assets/")
+
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	return r
+}
+
+func main() {
+
+	r := newRouter()
 	http.ListenAndServe(":8080", r)
 }
 
